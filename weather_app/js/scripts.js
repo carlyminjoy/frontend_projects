15697b57a408d12757f74ccde723ca47169a92b5
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-13 09:17:16 +1000

    added weather icon, updated html&css for centred layout

--- weather_app/js/scripts.js
+++ weather_app/js/scripts.js
@@ -15,8 +15,10 @@ function getLocation() {
 function getWeather(lon, lat) {
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=dc6b443deceda02a18b37174cc92c4fc&lat=-27.3944245&lon=153.074884&lat="+ lat + "&lon=" + lon,
   function(data) {
+    var icon = data.weather[0].icon;
     html = "<h3>" + data.name + "</h3>" + "<br>" +
-           "<h4>Weather: " + data.weather[0].main + "</h4>"
+           "<h4>" + data.weather[0].main + "</h4>"
     $("#location").html(html);
+    $("#icon").html("<img src=\"http://openweathermap.org/img/w/" + icon + ".png\">");
   })
 }
