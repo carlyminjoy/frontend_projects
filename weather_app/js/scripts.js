91e25bcc27196827bd19a1efc82619c655297ff2
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-13 12:59:46 +1000

    tidied getWeather function, fixed link issue to retrieve icon from API

--- weather_app/js/scripts.js
+++ weather_app/js/scripts.js
@@ -16,8 +16,10 @@ function getWeather(lon, lat) {
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=dc6b443deceda02a18b37174cc92c4fc&lat=-27.3944245&lon=153.074884&lat="+ lat + "&lon=" + lon,
   function(data) {
     var icon = data.weather[0].icon;
-    html = "<h3>" + data.name + "</h3>" + "<br>" +
-           "<h4>" + data.weather[0].main + "</h4>"
+    html = "<p>Where are ya? </p>" +
+           "<h3><strong>" + data.name + "</strong></h3>" + "<br>" +
+           "<p>View from the window today: <br></p>" +
+           "<h3><strong>" + data.weather[0].main + "</strong></h3>"
     $("#location").html(html);
     $("#icon").html("<img src=\"http://openweathermap.org/img/w/" + icon + ".png\">");
   })
