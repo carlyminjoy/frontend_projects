55f839aba62dc49c401640d895d450d968e572d8
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-14 09:56:21 +1000

    added button functionality for fahToCelsius method, reorganised code - less js generated html

--- weather_app/js/scripts.js
+++ weather_app/js/scripts.js
@@ -2,7 +2,21 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     getLocation();
   }
+  var temp = 0;
 });
+
+$('#toggle').click(function() {
+  temp = Math.floor(fahToCel(temp));
+  $("#temp").html("<p>" + temp + " degrees celsius</p>");
+})
+
+function fahToCel(f) {
+  return (f - 32) * (5/9);
+}
+//
+// function celToFah(c, f) {
+//
+// }
 
 function getLocation() {
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -16,11 +30,14 @@ function getWeather(lon, lat) {
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=dc6b443deceda02a18b37174cc92c4fc&lat=-27.3944245&lon=153.074884&lat="+ lat + "&lon=" + lon,
   function(data) {
     var icon = data.weather[0].icon;
-    html = "<p>Where are ya? </p>" +
-           "<h3><strong>" + data.name + "</strong></h3>" + "<br>" +
-           "<p>View from the window today: <br></p>" +
-           "<h3><strong>" + data.weather[0].main + "</strong></h3>"
-    $("#location").html(html);
+    temp = Math.floor(data.main.temp);
+    $("#location").html(
+      "<h3><strong>" + data.name + "</strong></h3>" + "<br>"
+    );
+    $("#weather").html(
+      "<h3><strong>" + data.weather[0].main + "</strong></h3>"
+    );
+    $("#temp").html("<h3>" + temp + " degrees fahrenehit</h3>");
     $("#icon").html("<img src=\"http://openweathermap.org/img/w/" + icon + ".png\">");
   })
 }
