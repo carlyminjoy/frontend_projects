6eb7ff8f8d46288bea1f58005be499524e4a7ef1
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-18 15:07:31 +1000

    added functions to retrieve data from mediawiki API and generate divs to display results

--- wikipedia_viewer/js/index.js
+++ wikipedia_viewer/js/index.js
@@ -3,5 +3,19 @@ function openRandomArticle() {
 }
 
 $("form").submit(function() {
-  $(".results").html("<p>RESULTS</p>");
-})
+  var input = $(".search-box").val();
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=" + input + "&origin=*";
+
+  $.getJSON(url, function(data) {
+    console.log(url);
+    for (var i = 0; i < 5; i++) {
+      $(".results").append(
+          "<a href=" + data[3][i] + " target='_blank'>" +
+          "<div>" +
+          "<h3>" + data[1][i] + "</h3>" +
+          "<p>" + data[2][i] + "</p></div></a>" )
+    };
+  })
+
+  return false;
+});
