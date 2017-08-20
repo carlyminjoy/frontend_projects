ede6d4296f21c33c35acfbb626608dcdfbc81f29
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-20 21:29:40 +1000

    Fixed codacy issues

--- wikipedia_viewer/js/index.js
+++ wikipedia_viewer/js/index.js
@@ -1,5 +1,5 @@
 function openRandomArticle() {
-  window.open("https://en.wikipedia.org/wiki/Special:Random")
+  window.open("https://en.wikipedia.org/wiki/Special:Random");
 }
 
 $("form").submit(function() {
@@ -8,23 +8,19 @@ $("form").submit(function() {
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=10&namespace=0&format=json&search=" + input + "&origin=*";
 
   $.getJSON(url, function(data) {
-    if (data[1].length == 0) {
+    if (data[1].length === 0) {
       $(".results").append(
         "<h4>No matches.</h4>" );
     } else {
       for (var i = 0; i < 5; i++) {
-        var link = data[3][i];
-        var title = data[1][i];
-        var blurb = data[2][i];
-
         $(".results").append(
-            "<a href=" + link + " target='_blank'>" +
+            "<a href=" + data[3][i] + " target='_blank'>" +
             "<div>" +
-            "<h3>" + title + "</h3>" +
-            "<p>" + blurb + "</p></div></a>" );
+            "<h3>" + data[1][i] + "</h3>" +
+            "<p>" + data[2][i] + "</p></div></a>" );
         }
-    };
-  })
+    }
+  });
 
   return false;
 });
