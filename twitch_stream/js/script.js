ac24225d7c6da583ebddd6b9913eb985d79b5c94
Carly Minjoy <cminjoy@myune.edu.au>
2017-08-29 00:18:35 +1000

    added functionality to retrieve online/offline status & click to view streaming channels

--- twitch_stream/js/script.js
+++ twitch_stream/js/script.js
@@ -1,47 +1,26 @@
-// var twitchUsers = [ "ESL_SC2", "OgamingSC2", "cretetion",
-//                     "freecodecamp", "storbeck", "habathcx",
-//                     "RobotCaleb", "noobs2ninjas"  ];
-
 var twitchUsers = { 'freecodecamp': 79776140, 'esl_sc2': 30220059,
                     'storbeck': 152475255, 'ogamingsc2': 71852806,
                     'habathcx': 6726509, 'cretetion': 90401618,
                     'noobs2ninjas': 82534701, 'robotcaleb': 54925078 };
 
-// var channelsURL = "https://wind-bow.gomix.me/twitch-api/channels/"
 var streamsURL = "https://wind-bow.gomix.me/twitch-api/streams/"
 var callback = "?callback=?";
 
-function getUserDetails(user) {
+function getStream(user) {
   $.getJSON(streamsURL + user + callback, function(stream) {
     if (stream.stream) {
-      $(".channels").append("<p>" + user + twitchUsers[user] + "Live.</p>")
+      $(".channels").append("<a href='"+stream.stream.channel.url+"' target=_'blank'><div class='live'><h3>" + user + "</h3>" +
+                            "<p>" + stream.stream.game + "</p></div></a>");
+
     } else {
-      $(".channels").append("<p>" + user + twitchUsers[user] + "Offline.</p>")
+      $(".channels").append("<div class='offline'><h3>" + user + "</h3>" +
+                            "<p>Offline</p></div>");
     }
   });
 }
 
 $(document).ready(function() {
   for (var user in twitchUsers) {
-    getUserDetails(user);
+    getStream(user);
   };
-    // var id        = 0;
-    // var username  = "";
-    // var string    = "";
-
-    // $.getJSON(channelsURL + twitchUsers[user] + callback, function(json) {
-    //   username  = json.name;
-    //   id        = json._id;
-    //   $(".channels").append(
-    //     "<p id = " + id + ">" + username + ": " + id+ "</p>" );
-    // });
-
-    // $.getJSON(streamsURL + id + callback, function(stream) {
-    //   if (stream.stream) {
-    //     string = username + " ID (" + id + "): Live stream";
-    //   } else {
-    //     string = username + " ID (" + id + "): Offline";
-    //   };
-    //   $(".channels").append("<p>" + string + "</p>");
-    // });
 });
